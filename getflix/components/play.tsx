cb9cd2ca804cfe958b834ca39c3d395837bd5c52
conradkg1 <conradkg1@gmail.com>
2023-05-18 20:18:27 -0500

    play.tsx tweaks

--- getflix/components/play.tsx
+++ getflix/components/play.tsx
@@ -2,3 +2,40 @@ import React from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { useRouter } from 'next/router';
 
+interface PlayButtonProps {
+    movieId: string;
+  }
+  
+  const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+    const router = useRouter();
+  
+    const handlePlayClick = () => {
+      router.push(`/watch/${movieId}`);
+    };
+  
+    return (
+      <button onClick={handlePlayClick} className="">
+        <BsFillPlayFill className="" />
+        Play
+      </button>
+    );
+  };
+
+// interface PlayButtonProps {
+//     movieId: string ;
+//   }  
+//   const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+//     const router = useRouter();
+  
+//     return (
+//       <button 
+//         onClick={() => router.push(`/watch/${movieId}`)}
+//         className=""
+//         >
+//           <BsFillPlayFill className="" />
+//           Play
+//       </button>
+//     );
+//   }
+
+  export default PlayButton;
